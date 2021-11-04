22cdfaeec31fe20118513c8109d233bdc47b3435
Michael0814772 <michael232akin@gmail.com>
2021-11-04 07:57:20 +0000

    second commit-1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/navbar/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+import Blog from './components/Blog';
+import Contact from './components/Contact';
+import Projects from './components/Projects';
+
 
 
 function App() {
@@ -9,7 +15,11 @@ function App() {
       <Router>
         <Navbar />
         <Switch>
-          
+          <Route path='./' components={Home}/>
+          <Route path='./about' components={About}/>
+          <Route path='./blog' components={Blog}/>
+          <Route path='./contact' components={Contact}/>
+          <Route path='./projects' components={Projects}/>
         </Switch>
       </Router>
     </div>
